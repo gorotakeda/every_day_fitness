aebc2d264ff2321c5f3b6a3d81626753e2583eb1
gorotakeda <rugbytkg3@gmail.com>
2021-08-03 16:25:02 +0900

    ログイン、ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         with_options presence: true do
+           validates :nickname
+           validates :profile
+           with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' } do
+             validates :last_name
+             validates :first_name
+           end
+        end
+         validates :password,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers.' }
 end
