0cc4265027b6c546405e5fa2ea2c686a55f78c2b
gorotakeda <rugbytkg3@gmail.com>
2021-08-08 17:13:09 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_one_attached :image
   has_many :fitnesses
+  has_many :room_users
+  has_many :rooms, through: :room_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
